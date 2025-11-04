fdd09e73fe37350975411396818f4f4fdaa52aa5
joegrice <joe.grice@hotmail.co.uk>
2025-11-04 21:33:04 +0000

    Fix loading schedule config

--- src/Configuration/CorsOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace TrainChecker.Configuration;
-
-public class CorsOptions
-{
-    public const string Cors = "Cors";
-    
-    public string[] AllowedOrigins { get; set; } = Array.Empty<string>();
-}
